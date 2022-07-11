3f0c0d6f8479412d4c2cde9beef0175f36a14935
German Overst <germanov013@gmail.com>
2022-07-11 11:43:55 -0300

    cambios en mostrar mensaje

--- M1/U 4/ejercicio 1.js
+++ M1/U 4/ejercicio 1.js
@@ -3,21 +3,21 @@ distancia = prompt("Ingrese la distancia a recorrer:", '');
 distancia = parseInt(distancia)
 
 if (distancia >= 0 && distancia <= 1000) {
-    alert('Es mejor recorrer a pie');
-    document.write('Es beneficioso para su salud')
+    alert('Es mejor recorrer a pie.'); document.write('Es beneficioso para su salud.')
+    
 } else {
     if (distancia >= 1000 && distancia <= 10000) {
-        alert('Es mejor utilizar la bicicleta');
+        alert('Es mejor utilizar la bicicleta.'); document.write('Muy bien. Haga ejercicios.')
 
     } else {
         if (distancia >= 10000 && distancia <= 30000) {
-            alert('Es necesario utilizar el colectivo')
+            alert('Es necesario utilizar el colectivo.');document.write('Si está apurado, siga este consejo.')
         } else {
             if (distancia >= 30000 && distancia <= 100000) {
-                alert('Es más cómodo ir en su auto')
+                alert('Es más cómodo ir en su auto.');document.write('Usted decide cuándo y cómo salir.')
             } else {
                 if (distancia >= 100000) {
-                    alert('No maneje tantos kilómetros, vaya en AVIÓN')
+                    alert('No maneje tantos kilómetros, vaya en AVIÓN.'); document.write('No hacemos préstamos. Sólo aconsejamos.')
                 }
             }
         }
